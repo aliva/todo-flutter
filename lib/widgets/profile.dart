9efa4d9c40a31d4d3042b4b682a5658a6134f4eb
Ali Vakilzade <ali@vakilzade.com>
2022-05-21 19:44:51 +0100

    use connectio state #12

--- lib/widgets/profile.dart
+++ lib/widgets/profile.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart';
-import 'package:todo/storage.dart';
 import 'package:todo/widgets/profile_signedin.dart';
 
 class Profile extends StatefulWidget {
@@ -16,9 +15,10 @@ class _ProfileState extends State<Profile> {
   Widget build(BuildContext context) {
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
-      initialData: Storage.currentUser,
       builder: (BuildContext context, AsyncSnapshot<User?> snapshot) {
-        if (snapshot.hasData) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Container();
+        } else if (snapshot.hasData) {
           return const Center(child: ProfileSignedin());
         } else {
           return const SignInScreen(
